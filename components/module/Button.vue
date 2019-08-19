fdb51962aaa3c086cebeb48fcfe484bfc8e456c0
matsumana07384 <manami.m.227@outlook.jp>
2019-08-19 22:12:18 +0900

    wip

---
+++ components/module/Button.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <v-btn
+    rounded
+    min-width = "370px"
+    min-height = "54px"
+    color = "#fff"
+    href = "https://google.com"
+    target="_blank"
+   >
+    <span class="button-text">
+      {{ text }}
+    </span>
+  </v-btn>
+</div>
+</template>
+<script>
+export default {
+  props: ['text','link']
+  
+}
+</script>
+<style scoped>
+.button-text {
+  font-family: HiraginoSans-W7;
+  font-size: 18px;
+  text-align: center;
+  color: #ff6f61;
+}
+</style>
