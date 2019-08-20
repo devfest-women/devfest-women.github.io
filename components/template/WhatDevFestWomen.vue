f558030e51c4854dd48741ccbf8b09307ad6430d
riho.takagi <rihohearts@gmail.com>
2019-08-20 17:52:25 +0900

    vuetifyのgridを使ってみた

--- components/template/WhatDevFestWomen.vue
+++ components/template/WhatDevFestWomen.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="about">
-    <div class="about-container d-flex flex-column justify-center align-center">
-      <SessionTitle text="DevFest Womenとは"></SessionTitle>
-      <div class="content">
-        DevFest WomenはIT業界において、ダイバーシティとインクルージョンに興味のある方のためのテクノロジーカンファレンスです。
-        エンジニア、デザイナー、ディレクター、プロダクトマネージャーなど様々な職種の方にとって、自分のなりたい像がみつかったり、自分の知っていることやできることを増やして明日からの自信につなげるイベントを目指しています。
-      </div>
-      <div class="hashtag">
-        <span class="tag">ハッシュタグ</span>
-        <a href="https://twitter.com/search?q=%23DevFestWomen" class="link">#DevFestWomen</a>
-      </div>
-    </div>
+  <div class="bg">
+    <v-container class="about-container d-flex flex-column justify-center align-center">
+      <v-row justify="center">
+        <v-col lg="6">
+          <SessionTitle text="DevFest Womenとは"></SessionTitle>
+          <div class="content">
+            DevFest WomenはIT業界において、ダイバーシティとインクルージョンに興味のある方のためのテクノロジーカンファレンスです。
+            エンジニア、デザイナー、ディレクター、プロダクトマネージャーなど様々な職種の方にとって、自分のなりたい像がみつかったり、自分の知っていることやできることを増やして明日からの自信につなげるイベントを目指しています。
+          </div>
+          <div class="hashtag d-flex justify-center align-center">
+            <span class="tag">ハッシュタグ</span>
+            <a href="https://twitter.com/search?q=%23DevFestWomen" class="link">#DevFestWomen</a>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>>
   </div>
 </template>
 
@@ -31,8 +35,6 @@
 
 <style lang="scss" scoped>
   .about-container{
-    max-width: 1280px; //FIXME: 共通化したほうがいいかも？でもどこの階層にどう入れるのがいいかわからない
-    margin: 0px 400px 0px 400px; //FIXME: 共通化したほうがいいかも？でもどこの階層にどう入れるのがいいかわからない
     padding: 45px 0px 45px 0px;
   }
 
@@ -44,7 +46,6 @@
     margin: 0px 0px 30px 0px;
   }
   .hashtag{
-    display: flex;
     .tag{
       color: #7a7a8c;
       font-family: HiraginoSans-W6;
