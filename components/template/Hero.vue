fa7d0ed3848dea677973d6f4e378e56324789ddb
matsumana07384 <manami.m.227@outlook.jp>
2019-08-26 22:58:15 +0900

    Heroの調整

--- components/template/Hero.vue
+++ components/template/Hero.vue
@@ -3,9 +3,9 @@
   <Header class="header"></Header>
   <div class="contents">
     <div class="hero-logo" justify-center="center">
-      <img src="~/assets/img/wtm_logo_white.svg">
+      <img class="footer-logo" src="~/assets/img/logo_white.svg" alt="DevFest Women2019">
       <p class="hero-days">{{ hero.days }}</p>   
-      <p >{{ hero.place }}</p>
+      <p class="hero-place">{{ hero.place }}</p>
     </div>
     <div class="hero-link">
       <p class="label-text">{{ hero.button.description }}</p>
@@ -98,6 +98,7 @@ import Button from '~/components/module/Button.vue'
   color: #ffffff;
 }
 .hero-place {
+  margin-top: -15px;
   font-family: HiraginoSans-W3;
   font-size: 15px;
   color: #ffffff;
@@ -108,7 +109,7 @@ import Button from '~/components/module/Button.vue'
   margin: 0;
   padding-top: 31px;
   position: relative;
-  top: 118px;
+  top: 123px;
   z-index: 10;
   background-color: rgba(255, 255, 255, 0.2);
 }
