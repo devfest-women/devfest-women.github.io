e82c504d6bfa9efc214f240734f5c267b65d2754
matsumana07384 <manami.m.227@outlook.jp>
2019-08-27 07:39:36 +0900

    ボタンのクラスを変更

--- components/module/Button.vue
+++ components/module/Button.vue
@@ -23,7 +23,7 @@
     min-height = "54px"
     href = "https://google.com"
     target="_blank"
-    style="background-image: linear-gradient(to left, #da297b, #ff6f61);"
+    class="button-gradesion"
    >
     <span class="button-text color-white">
       {{ text }}
@@ -37,11 +37,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.button-gradesion{
+  background-image: linear-gradient(to left, #da297b, #ff6f61);
+}
 .button-text {
   font-family: HiraginoSans-W7;
   font-size: 16px;
   text-align: center;
-  color: #ff6f61;
 }
 .color-white {
   color: #ffffff;
