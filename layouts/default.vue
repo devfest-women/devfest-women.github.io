f1673ed8789b76e40a8b10611345a14666402b7a
matsumana07384 <manami.m.227@outlook.jp>
2019-08-27 13:01:53 +0900

    不要な処理を削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,34 +8,11 @@
 
 <script>
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+
 }
 </script>
 <style>
   .default-setting {
     color: #ffffff;
-    padding:0;
   }
 </style>
