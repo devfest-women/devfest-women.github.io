e92adb0fe8401c11358248b7320a41fbc2b899a3
matsumana07384 <manami.m.227@outlook.jp>
2019-08-28 13:28:07 +0900

    レイアウトの記述を変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,61 @@
 <template>
-    <v-content class="default-setting">
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
+  <v-content>
+    <v-app>
+      <v-layout
+        column
+        justify-center
+        align-center
+      >
+        <v-flex
+          xs12
+          sm8
+          md6
+        >
+        <Header pages="top"></Header>
+          <v-container>
+            <nuxt />
+          </v-container>
+        <Footer class="bg-gradient l-footer"></Footer>
+        </v-flex>
+      </v-layout>
+    </v-app>
+  </v-content>    
 </template>
 
 <script>
-export default {
+import Header from '~/components/template/Header.vue'
+import Footer from '~/components/template/Footer.vue'
 
+export default {
+    components: {
+    Header,
+    Footer
+  }
 }
 </script>
 <style>
-  .default-setting {
-    color: #ffffff;
-  }
+.default-setting {
+  color: #ffffff;
+}
+.bg-black {
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+  background-color: #100e17;
+}
+.bg-gradient {
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+  background-image: linear-gradient(to left, #da297b, #ff6f61);
+}
+.l-footer{
+  margin-top: -12px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,36 @@
 <template>
-  <v-app>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
     <div class="text-xs-center">
-      <Hero class="bg_layout"></Hero>
-      <WhatDevFestWomen class="bg_black anchor" id="aboutdevfestwomen"></WhatDevFestWomen>
-      <About class="bg_black anchor" id="aboutevent"></About>
-      <Schedule class="bg_gradient anchor" id="schedule"></Schedule>
-      <Sponsor class="bg_black anchor" id="sponsor"></Sponsor>
-      <!-- <Staff class="bg bg_black"></Staff> -->
-      <Footer class="bg_gradient"></Footer>
+      <Hero class="bg-layout"></Hero>
+      <WhatDevFestWomen class="bg-black anchor" id="aboutdevfestwomen"></WhatDevFestWomen>
+      <About class="bg-black anchor" id="aboutevent"></About>
+      <Schedule class="bg-gradient anchor" id="schedule"></Schedule>
+      <Sponsor class="bg-black anchor" id="sponsor"></Sponsor>
+      <!-- <Staff class="bg bg-lack"></Staff> -->
     </div>
-    </v-flex>
-  </v-layout>
-  </v-app>
 </template>
 
 <script>
-import Header from '~/components/template/Header.vue'
 import Hero from '~/components/template/Hero.vue'
 import WhatDevFestWomen from '~/components/template/WhatDevFestWomen.vue'
 import About from '~/components/template/About.vue'
 import Schedule from '~/components/template/Schedule.vue'
 import Sponsor from '~/components/template/Sponsor.vue'
 import Staff from '~/components/template/Staff.vue'
-import Footer from '~/components/template/Footer.vue'
 
 export default {
   components: {
-    Header,
     Hero,
     WhatDevFestWomen,
     About,
     Schedule,
     Sponsor,
     Staff,
-    Footer
   }
 }
 </script>
 
-
 <style scoped>
-.bg_black {
-  width: 100vw;
-  position: relative;
-  left: 50%;
-  right: 50%;
-  margin-left: -50vw;
-  margin-right: -50vw;
-  background-color: #100e17;
-}
-.bg_gradient {
-  width: 100vw;
-  position: relative;
-  left: 50%;
-  right: 50%;
-  margin-left: -50vw;
-  margin-right: -50vw;
-  background-image: linear-gradient(to left, #da297b, #ff6f61);
-}
-.bg_layout{
+.bg-layout{
   margin-bottom: -12px;
 }
 .anchor{
