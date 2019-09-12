5d8b9b66055acb9f695b3be107e3246a744fcee6
Shoko Okochi <shoko.okochi@dena.com>
2019-09-12 10:07:17 +0900

    add uphyca

--- components/template/Sponsor.vue
+++ components/template/Sponsor.vue
@@ -21,48 +21,48 @@
                 <a href="https://cybozu.co.jp/"><img src="~/assets/img/sponsor/cybozu.png" alt="サイボウズ株式会社" title="サイボウズ株式会社"></a>
               </v-col>
               <!-- <v-col sm="6" class="silver-logo">
-                <img src="~/assets/img/sponsor/sansan.png" alt="Sansan株式会社" title="Sansan株式会社">
+                <a href=""><img src="~/assets/img/sponsor/sansan.png" alt="Sansan株式会社" title="Sansan株式会社"></a>
               </v-col> -->
             </v-row>
-            <!-- <v-row class="bronze d-flex flex-column flex-lg-row"> -->
-              <!-- <v-col sm="6" md="4" class="bronze-logo">
-                <img src="~/assets/img/sponsor/dena.png" alt="株式会社ディー・エヌ・エー" title="株式会社ディー・エヌ・エー">
-              </v-col>
-              <v-col xs="6" md="4" class="bronze-logo">
-                <img src="~/assets/img/sponsor/unity.png" alt="ユニティ・テクノロジーズ・ジャパン合同会社" title="ユニティ・テクノロジーズ・ジャパン合同会社">
-              </v-col>
-              <v-col xs="6" md="4" class="bronze-logo">
-                <img src="~/assets/img/sponsor/uphyca.png" alt="株式会社ウフィカ" title="株式会社ウフィカ">
-              </v-col> -->
-            <!-- </v-row> -->
             <v-row class="bronze d-flex flex-column flex-lg-row">
+              <!-- <v-col xs="6" md="4" class="bronze-logo">
+                <img src="~/assets/img/sponsor/unity.png" alt="ユニティ・テクノロジーズ・ジャパン合同会社" title="ユニティ・テクノロジーズ・ジャパン合同会社">
+              </v-col> -->
+              <!-- <v-col sm="6" md="4" class="bronze-logo">
+                <a href=""><img src="~/assets/img/sponsor/dena.png" alt="株式会社ディー・エヌ・エー" title="株式会社ディー・エヌ・エー"></a>
+              </v-col> -->
+              <v-col xs="6" md="4" class="bronze-logo">
+                <a href="https://www.uphyca.com/"><img src="~/assets/img/sponsor/uphyca.png" alt="株式会社ウフィカ" title="株式会社ウフィカ"></a>
+              </v-col>
+            </v-row>
+            <v-row class="bronze d-flex flex-column flex-lg-row">
+              <v-col xs="6" md="4" class="bronze-logo">
+                <a href="https://github.com/"><img src="~/assets/img/sponsor/github.png" alt="ギットハブ・ジャパン合同会社" title="ギットハブ・ジャパン合同会社"></a>
+              </v-col>
               <v-col xs="6" md="4" class="bronze-logo">
                 <a href="https://www.sketch.com/"><img src="~/assets/img/sponsor/sketch.png" alt="Sketch" title="Sketch"></a>
               </v-col>
               <v-col xs="6" md="4" class="bronze-logo">
                 <a href="https://www.styliste.com"><img src="~/assets/img/sponsor/styliste.png" alt="ファッションパートナー株式会社" title="ファッションパートナー株式会社"></a>
               </v-col>
-              <v-col xs="6" md="4" class="bronze-logo">
-                <a href="https://github.com/"><img src="~/assets/img/sponsor/github.png" alt="ギットハブ・ジャパン合同会社" title="ギットハブ・ジャパン合同会社"></a>
-              </v-col>
             </v-row>
-            <!-- <v-row class="personal d-flex flex-column flex-lg-row">
-              <v-col xs="4" md="2" class="personal-logo">
-                <img src="~/assets/img/sponsor/yuichi-yagami.png" alt="谷上雄一" title="谷上雄一">
-              </v-col>
-              <v-col xs="4" md="2" class="personal-logo">
-                <img src="~/assets/img/sponsor/tomoharu-nagasawa.png" alt="長沢智治" title="長沢智治">
-              </v-col>
-              <v-col xs="4" md="2" class="personal-logo">
-                <img src="~/assets/img/sponsor/takayuki-fuwa.png" alt="不破崇行" title="不破崇行">
-              </v-col>
-              <v-col xs="4" md="2" class="personal-logo">
-                <img src="~/assets/img/sponsor/satoru-nakamura.png" alt="なかむらさとる" title="なかむらさとる">
-              </v-col>
-              <v-col xs="4" md="2" class="personal-logo">
-                <img src="~/assets/img/sponsor/toshihisa-osawa.png" alt="大澤敏久" title="大澤敏久">
-              </v-col>
-            </v-row> -->
+            <v-row class="personal d-flex flex-column flex-lg-row">
+              <!-- <v-col xs="4" md="2" class="personal-logo">
+                <a href=""><img src="~/assets/img/sponsor/yuichi-yagami.png" alt="谷上雄一" title="谷上雄一"></a>
+              </v-col> -->
+              <!-- <v-col xs="4" md="2" class="personal-logo">
+                <a href=""><img src="~/assets/img/sponsor/tomoharu-nagasawa.png" alt="長沢智治" title="長沢智治"></a>
+              </v-col> -->
+              <!-- <v-col xs="4" md="2" class="personal-logo">
+                <a href=""><img src="~/assets/img/sponsor/takayuki-fuwa.png" alt="不破崇行" title="不破崇行"></a>
+              </v-col> -->
+              <!-- <v-col xs="4" md="2" class="personal-logo">
+                <a href=""><img src="~/assets/img/sponsor/satoru-nakamura.png" alt="なかむらさとる" title="なかむらさとる"></a>
+              </v-col> -->
+              <!-- <v-col xs="4" md="2" class="personal-logo">
+                <a href=""><img src="~/assets/img/sponsor/toshihisa-osawa.png" alt="大澤敏久" title="大澤敏久"></a>
+              </v-col> -->
+            </v-row>
           </v-container>
         </v-col>
       </v-row>
